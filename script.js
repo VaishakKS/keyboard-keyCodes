ef0f0df875d6d82b577f331876834545b0f4a9ac
VaishakKS <vaishakmnglr@gmail.com>
2021-09-24 18:14:59 +0000

    key codes dev completion

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ $(document).ready(function() {
     $(this).keydown(function(e) {
         $('.key').addClass('key-animate');
         let keyCode = e.code;
+        console.log(e)
         let codes = keyCode.match(/[A-Z][a-z]+/g);
         if (codes && codes.length >= 2) {
-            $('.key-select-text').text(codes.join(' '));
+            $('.key-select-text').text(`keyCode: ${codes.join(' ')}`);
+            $('.key-name').text(`Key pressed: ${e.key}`);
+            $('.key-code').text(`Code: ${e.keyCode}`);
         } else {
-            $('.key-select-text').text(e.code);
+            $('.key-select-text').text(`keyCode: ${e.code}`);
+            $('.key-name').text(`Key pressed: ${e.key}`);
+            $('.key-code').text(`Code: ${e.keyCode}`);
         }
         setTimeout(() => {
             $('.key').removeClass('key-animate');
