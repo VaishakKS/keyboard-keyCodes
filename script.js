271b563b1bc111032c009295ab82559d3daf6d0d
VaishakKS <vaishakmnglr@gmail.com>
2021-09-24 18:18:53 +0000

    code cleanup

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
     $(this).keydown(function(e) {
+        function setKeyCodes(e) {
+            $('.key-name').text(`Key pressed: ${e.key}`);
+            $('.key-code').text(`Code: ${e.keyCode}`);
+        }
         $('.key').addClass('key-animate');
         let keyCode = e.code;
-        console.log(e)
         let codes = keyCode.match(/[A-Z][a-z]+/g);
         if (codes && codes.length >= 2) {
             $('.key-select-text').text(`keyCode: ${codes.join(' ')}`);
-            $('.key-name').text(`Key pressed: ${e.key}`);
-            $('.key-code').text(`Code: ${e.keyCode}`);
+            setKeyCodes(e);
         } else {
             $('.key-select-text').text(`keyCode: ${e.code}`);
-            $('.key-name').text(`Key pressed: ${e.key}`);
-            $('.key-code').text(`Code: ${e.keyCode}`);
+            setKeyCodes(e);
         }
         setTimeout(() => {
             $('.key').removeClass('key-animate');
